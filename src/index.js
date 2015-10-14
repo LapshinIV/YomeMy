419e23e69aadf96caec64c690e38324ee99d695b
Ivan <ivan.lapshin@39parrot.net>
2015-10-14 09:38:01 +0400

    fix bug history

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 // Famous dependencies
 var DOMElement = require('famous/dom-renderables/DOMElement');
 var FamousEngine = require('famous/core/FamousEngine');
+var GestureHandler = require('famous/components/GestureHandler');
+var _ = require('underscore/underscore-min');
 
 // Boilerplate code to make your life easier
 FamousEngine.init();
@@ -10,12 +12,39 @@ FamousEngine.init();
 // Initialize with a scene; then, add a 'node' to the scene root
 var scene = FamousEngine.createScene();
 var bill  = scene.addChild();
-var personList = scene.addChild();
-var personList2 = personList.addChild();
+var personList = bill.addChild();
+var history = scene.addChild();
 
+
+
+var peoples = {
+    lennart: {
+        id: 'lelle',
+        faceImage: './images/lelle.jpg',
+        name: 'Lennart K?llb?ck'
+        },
+    dmitry:{
+        id: 'dmitry',
+        faceImage: './images/dmitry.jpeg',
+        name: 'Dmitry Kouznetsov'
+        },
+    alex: {
+        id: 'alex',
+        faceImage: './images/alex.jpg',
+        name: 'Alexey Emshanov'
+        },
+    ivan: {
+        id: 'ivan',
+        faceImage: './images/ivan.jpg',
+        name: 'Ivan Lapshin'
+        }
+};
+console.log(peoples);
 
 bill.setSizeMode ('relative', 'absolute')
-    .setAbsoluteSize (0, 100);
+    .setAbsoluteSize (0, 100)
+    .setProportionalSize(0.9, 1);
+
 var billDIV = new DOMElement(bill, {
     content: 300 + ' SEK',
     properties: {
@@ -27,10 +56,13 @@ var billDIV = new DOMElement(bill, {
         'border-bottom': '2px solid grey'
     }
 });
+
+var yPos = 100;
+
     personList.setSizeMode('relative', 'absolute')
-        .setPosition(0, 100)
+        .setPosition(0, yPos)
         .setAbsoluteSize(0, 65)
-        .setAlign(0,0);
+        .setAlign(0, 0);
 
     var personlistDIV = new DOMElement(personList, {
         content: ' ',
@@ -42,9 +74,28 @@ var billDIV = new DOMElement(bill, {
     });
 
     var face = personList.addChild();
+
     face.setSizeMode('absolute', 'absolute')
         .setAbsoluteSize(48, 48)
         .setPosition(5, 5);
 
     var faceDIV = new DOMElement(face, {tagName: 'img', properties: {'border-radius': 24 + 'px'}})
-        .setAttribute('src', './images/lelle.jpg');
+        .setAttribute('src', peoples.lennart.faceImage);
+    console.log('hi');
+
+
+history.setAlign(0.9, 0);
+
+var historyDIV = new DOMElement(history,{
+    properties: {
+        'background-color': 'lightblue',
+        'padding': '10px'
+    }});
+
+history.gestures = new GestureHandler(history);
+history.gestures.on('drag', function(e,p) {
+    var currentPos = history.getPosition();
+    var newPosX = currentPos[0] + e.centerDelta.x;
+    history.setPosition(newPosX, currentPos[1]);
+    console.log('hi2');
+});
